5e916852b0106dde5860f84dca305c4a5712adf6
Leviathan173 <hft0149@Gmail.com>
2019-10-17 21:56:36 +0800

    添加r18的选项和一个新的选项用以过滤R18G之类的内容

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ import spider
 parser = argparse.ArgumentParser(description='涩图获取器 v0.1c by Leviathan173')
 parser.add_argument('-i', '--index', help='需要爬取的页面的开始序号，默认77100000', type=int)
 parser.add_argument('-l', '--limit', help='最小被人喜欢的数值，只会爬取超过这个数值的涩图', type=int)
-parser.add_argument('-r18', help='是否只抓取涩图，yes or no')
+parser.add_argument('-r18', help='是否只抓取涩图,1=是,2=否,3=我全都要', type=int)
+parser.add_argument('-ds', '--darkside', help='可怕的，不被大众接受的，那些你不会告诉别人的爱好，上帝不会原谅你的,0=关闭,1=开启', type=int)
 parser.add_argument('-n', '--num', help='要爬取多少张图片', type=int)
 # TODO 添加数据库选项支持
 # 目前不知道怎么做匿名数据库支持，
@@ -37,13 +38,10 @@ else:
     print('limit={}'.format(limit))
 
 if args.r18 is None:
-    r18 = True
+    r18 = 3
     print('no specific argument given, ues default value True for r18')
 else:
-    if args.r18 == 'yes':
-        r18 = True
-    else:
-        r18 = False
+    r18 = args.r18
     print('r18={}'.format(r18))
 print(type(args.r18))
 
@@ -54,13 +52,20 @@ else:
     num = args.num
     print('times={}'.format(num))
 
+if args.darkside is None:
+    ds = 0
+    print("it's still be good")
+else:
+    ds = args.darkside
+    print(f'ds = {ds}')
+
 
 def main():
     print('载入中...')
     time.sleep(3)
     ind = index
     for i in range(num):
-        ind = spider.spider(ind, r18, limit)
+        ind = spider.spider(ind, r18, limit, ds)
 
 
 if __name__ == '__main__':
--- spider.py
+++ spider.py
@@ -6,12 +6,12 @@ from bs4 import BeautifulSoup
 from util import *
 
 
-def spider(index, r18, limit):
+def spider(index, r18, limit, ds):
     """大致流程
     1、request获取pixiv.net/artwork/index的数据
     2、判断是否为要求的
-    3、如果是，下载，返回index+1
-    如果不是 index+1，返回1
+    3、如果是，下载，index+1,返回index
+    如果不是 index+1，重试
     """
 
     while True:
@@ -19,25 +19,6 @@ def spider(index, r18, limit):
         url = f"https://www.pixiv.net/artworks/{index}"
         print(f'正在开始获取{url}的数据...')
 
-        '''try:
-            # 如果你的网络可以直接访问pixiv
-            # 那么你将不需要下面的几行代码
-            # 可以删除28行一行
-            # 将29行中的verify=False去掉
-            requests.packages.urllib3.disable_warnings()
-            res = requests.get(url, verify=False, timeout=(10, 30))
-            res = get_data(url)
-            if res.status_code in code:
-                print('页面不存在...')
-                index += 1
-                continue
-            soup = BeautifulSoup(res.text, 'html.parser')
-        except requests.exceptions.RequestException as e:
-            print(e)
-            print('超时...')
-            print('跳过，进入下一任务...')
-            index += 1
-            continue'''
         res = get_data(url)
         if res == -1:
             print(f'在重试了{RETRY_TIME}次之后依旧不能获取数据，跳过..')
@@ -57,6 +38,30 @@ def spider(index, r18, limit):
             index += 1
             continue
 
+        # 判断是否不可名状
+        ds_flag = False
+        for r in fatal_error:
+            if r in tags:
+                ds_flag = True
+        if ds == 0 and ds_flag:
+            print("发现不可名状之物...")
+            index += 1
+            continue
+        elif ds == 1 and not ds_flag:
+            print("是正常世界...")
+            index += 1
+            continue
+        elif ds == 1 and ds_flag:
+            print('发现符合，正在获取图片链接...')
+            img_url = get_img_url(soup.text)
+            for t in range(RETRY_TIME):
+                if dl(img_url, hash_name(get_ill_name(soup.text)), url) == 0:
+                    print('下载完成...')
+                    index += 1
+                    write_index(index)
+                    return index
+                else:
+                    continue
         # 判断是否R18
         r18_flag = False
         tag_flag = False
@@ -70,7 +75,7 @@ def spider(index, r18, limit):
                 tag_flag = True
 
         # 判断是否符合要求
-        if r18:
+        if r18 == 1:
             if r18_flag:
                 if tag_flag:
                     print('发现符合，正在获取图片链接...')
@@ -91,13 +96,28 @@ def spider(index, r18, limit):
                 print('不是R18，跳过...')
                 index += 1
                 continue
-        else:
+        elif r18 == 2:
             if tag_flag:
                 if r18_flag:
                     print('存在R18，跳过')
                     index += 1
                     continue
                 else:
+                    print('发现符合，正在获取图片链接...')
+                    img_url = get_img_url(soup.text)
+                    for t in range(RETRY_TIME):
+                        if dl(img_url, hash_name(get_ill_name(soup.text)), url) == 0:
+                            print('下载完成...')
+                            index += 1
+                            write_index(index)
+                            return index
+                        else:
+                            print('下载出错，重试中...')
+                            continue
+                    index += 1
+                    return index
+            elif r18 == 3:
+                if tag_flag:
                     print('发现符合，正在获取图片链接...')
                     img_url = get_img_url(soup.text)
                     for t in range(RETRY_TIME):
